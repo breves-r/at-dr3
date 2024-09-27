15aed5cc09292cce41aa0a16d7dd41f8bfdb013f
Deborah Wyny <rafaela.curty13@gmail.com>
2024-09-26 23:08:16 -0300

    teste deploykube v3

--- chefe-service/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO CHEFES(nome, sobrenome) VALUES ('Duda', 'MAFRA');
-INSERT INTO CHEFES(nome, sobrenome) VALUES ('Duda', 'SOUZA');
-INSERT INTO CHEFES(nome, sobrenome) VALUES ('Rebeca', 'MAFRA');
-INSERT INTO CHEFES(nome, sobrenome) VALUES ('Debora', 'Menezes');
